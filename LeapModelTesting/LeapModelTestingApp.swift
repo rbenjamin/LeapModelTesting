9ecc63e023bc22a52a4bb3a56087087238c3d549
Ben Davis <ben@pinwheeldevinc.com>
2026-03-13 15:41:18 -0400

    Initial Commit

---
+++ LeapModelTesting/LeapModelTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeapModelTestingApp.swift
+//  LeapModelTesting
+//
+//  Created by Ben Davis on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct LeapModelTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeapModelTestingTests/LeapModelTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  LeapModelTestingTests.swift
+//  LeapModelTestingTests
+//
+//  Created by Ben Davis on 3/13/26.
+//
+
+import Testing
+@testable import LeapModelTesting
+
+struct LeapModelTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
